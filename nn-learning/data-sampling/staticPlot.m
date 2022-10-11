2a58aa4d4aed66e17b320e32490816c05095aa0a
Michael <inflex56@gmail.com>
2022-10-11 11:22:29 +0200

    upd

--- nn-learning/data-sampling/staticPlot.m
+++ nn-learning/data-sampling/staticPlot.m
@@ -10,7 +10,9 @@ ax_body.XLim = [-0.9 0.9];
 ax_body.YLim = [-0.9 0.9];
 ax_body.ZLim = [-0.3 1.3];
 patch_handle = [];
-load('meshes/mesh_light_ord2.mat');
+load('meshes/mesh_light.mat');
+load('meshes/mesh_full.mat');
+
 base = eye(4);
 q_min = [-2.8973 -1.7628 -2.8973 -3.0718 -2.8973 -0.0175	-2.8973 0];
 q_max = [2.8973	1.7628	2.8973	-0.0698	2.8973	3.7525	2.8973 0.04];
